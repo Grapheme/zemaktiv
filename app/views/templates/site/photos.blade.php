e2b1b4b9aad5acb7308e532683ad1e2dc1568cbb
Владимир <vkharseev@gmail.com>
2015-07-23 12:13:49 +0300

    Шаблоны

---
+++ app/views/templates/site/photos.blade.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * TITLE: Фотографии
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+@section('style')
+@stop
+@section('content')
+    {{ $page->draw_blocks() }}
+@stop
+@section('scripts')
+@stop
