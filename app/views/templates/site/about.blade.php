a56758935557c9586b70033ddd9f3409ea24bd7e
Владимир <vkharseev@gmail.com>
2015-07-23 12:13:05 +0300

    Шаблоны

---
+++ app/views/templates/site/about.blade.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * TITLE: О проекте
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+@section('style')
+@stop
+@section('content')
+    {{ $page->draw_blocks() }}
+@stop
+@section('scripts')
+@stop
