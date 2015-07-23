4480c170761ff4059f1971d88668ebdc52d2c23f
Владимир <vkharseev@gmail.com>
2015-07-23 12:08:48 +0300

    Главное меню

--- app/views/templates/site/header.blade.php
+++ app/views/templates/site/header.blade.php
@@ -19,25 +19,8 @@
     </div>
     <div class="header__menu">
         <nav class="menu__nav">
+            {{ Menu::placement('main_menu') }}
             <ul>
-                <li class="nav__item item-us item-raspb">
-                    <a href="{{ pageurl("choice-land") }}" {{ Helper::isRoute('page','choice-land') }} class="item__link"><span>Выбор участка</span></a>
-                </li>
-                <li class="nav__item item-us item-green">
-                    <a href="{{ pageurl("buildings") }}" {{ Helper::isRoute('page','buildings') }} class="item__link"><span>Готовые дома</span></a>
-                </li>
-                <li class="nav__item item-us item-blue">
-                    <a href="{{ pageurl("communications") }}" {{ Helper::isRoute('page','communications') }} class="item__link"><span>Коммуникации</span></a>
-                </li>
-                <li class="nav__item item-us item-red">
-                    <a href="{{ pageurl("location") }}" {{ Helper::isRoute('page','location') }} class="item__link"><span>Расположение</span></a>
-                </li>
-                <li class="nav__item item-us item-yellow">
-                    <a href="{{ pageurl("photos") }}" {{ Helper::isRoute('page','photos') }} class="item__link"><span>Фотографии</span></a>
-                </li>
-                <li class="nav__item item-us item-orange">
-                    <a href="{{ pageurl("about") }}" {{ Helper::isRoute('page','about') }} class="item__link"><span>О проекте</span></a>
-                </li>
                 <li class="nav__item item-call">
                     <a href="#" class="item__link"><span>Заказать звонок</span></a>
                 </li>
