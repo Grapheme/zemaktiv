7b99ddc78dfdfa431cfecd2aaf0d62ec6034fa5f
KostyaD <thedamaxstudio@gmail.com>
2015-07-24 17:35:45 +0300

    comms

--- public/theme/src/scripts/main.js
+++ public/theme/src/scripts/main.js
@@ -17,8 +17,38 @@ Garden.header = function() {
 	$(window).on('scroll', setHeader);
 	setHeader();
 }
+Garden.indexSlider = function() {
+	if(!$('.js-index-slider').length) return;
+	var parent = $('.js-index-slider');
+	var slides = parent.find('.js-slide');
+	var dotsDiv = parent.find('.js-dots');
+	var zTimeout;
+	var show = function(id) {
+		$.each([slides, dotsDiv.find('.js-dot')], function(index, value){
+			value.eq(id).addClass('active')
+				.siblings().removeClass('active');
+		});
+		clearTimeout(zTimeout);
+		zTimeout = setTimeout(function(){
+			slides.eq(id).addClass('zActive')
+				.siblings().removeClass('zActive');
+		}, 500);
+	}
+	var init = function() {
+		for(var i = 0; i < slides.length; i++) {
+			dotsDiv.append('<li class="js-dot"></li>');
+		}
+		show(0);
+		$('.js-dot').on('click', function(){
+			show($(this).index());
+			return false;
+		});
+	}
+	init();
+}
 Garden.init = function() {
 	this.header();
+	this.indexSlider();
 }
 $(function(){
 	Garden.init();
