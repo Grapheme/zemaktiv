ad208f5f66b7b76cb1401c870d2c26c34e41f41b
Владимир <vkharseev@gmail.com>
2015-07-24 12:12:50 +0300

    Гость. Шаблон главной страницы

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -23,6 +23,10 @@
             endif;
         endforeach;
     endif;
+    $gallery = array();
+    if($buildings_photos = Buildings::where('photo_id','>',0)->orderByRaw("RAND()")->take(25)->lists('photo_id')):
+        $gallery = Photo::whereIn('id', $buildings_photos)->lists('name');
+    endif;
 ?>
 
 <main class="main js-main">
@@ -54,7 +58,7 @@
     <div class="index-block">
         <div class="wrapper">
             <div class="block__map-icon"></div>
-            <div class="block__text">Симферопольское шоссе, 70 км от МКАД</div>
+            <div class="block__text">{{ $page->block('card') }}</div>
             <div class="block__btn"><a href="{{ pageurl('about') }}" class="us-btn btn-white"><span>Карта</span></a></div>
         </div>
     </div>
@@ -70,7 +74,7 @@
         <div class="wrapper">
             <div class="block__title">Коммуникации</div>
             <div class="block__text text-min">
-                Газ, электричество, водопровод входят в стоимость земельного участка.<br>Забор с калиткой и воротами тоже.
+                {{ $page->block('communication') }}
             </div>
             <div class="block__btn"><a href="{{ pageurl('communications') }}" class="us-btn btn-blue"><span>Узнать подробнее</span></a></div>
         </div>
@@ -94,51 +98,28 @@
         <div class="block-container">
             <div class="wrapper">
                 <div class="block__title">Выбор участка</div>
-                <div class="block__text text-min">Вы можете выбрать участок на карте либо подобрать подходящий лично
-                    вам<br>по
-                    параметрам и тех. особенностям. От 9 до 25 соток.
+                <div class="block__text text-min">
+
                 </div>
                 <div class="block__btn"><a href="#" class="us-btn btn-green"><span>Подбор участка</span></a></div>
             </div>
         </div>
         <div class="block-line">
             <div class="wrapper">
-                <div class="line__inside"><b>Готовность:</b><span>высажено 16 декоративнолиственных кустарников, 14 фруктовых деревьев.</span>
+                <div class="line__inside"><b>Готовность:</b><span>{{ $page->block('readiness') }}</span>
                 </div>
             </div>
         </div>
     </div>
+@if(count($gallery))
     <div class="index-gallery">
-        <div class="gallery__line"><a href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                                      class="line__item"></a><a href="#"
-                                                                style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                                                                class="line__item"></a><a href="#"
-                                                                                          style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                                                                                          class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a><a
-                    href="#" style="background-image: url(images/tmp/index-bottom-slider1.jpg);"
-                    class="line__item"></a>
+        <div class="gallery__line">
+        @foreach($gallery as $photo)
+            <a href="javascript:void(0);" style="background-image: url({{ asset(Config::get('site.galleries_photo_public_dir').'/'.$photo) }});" class="line__item"></a>
+        @endforeach
         </div>
     </div>
+@endif
 </main>
 @stop
 @section('scripts')
