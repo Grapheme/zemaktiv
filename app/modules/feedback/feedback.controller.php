b729c5f929c313db37a9186547de31b264fb6dd0
Владимир <vkharseev@gmail.com>
2015-08-04 12:18:40 +0300

    Шаблон Бронирование участка

--- app/modules/feedback/feedback.controller.php
+++ app/modules/feedback/feedback.controller.php
@@ -12,6 +12,7 @@ class FeedbackController extends BaseController {
         $class = __CLASS__;
         Route::post("/contacts/feedback", array('as' => 'contact_feedback', 'uses' => $class . "@postContactFeedback"));
         Route::post("/request/call", array('as' => 'request_call', 'uses' => $class . "@requestCall"));
+        Route::post("/request/bron", array('as' => 'request_bron', 'uses' => $class . "@requestBron"));
 
     }
 
@@ -37,9 +38,36 @@ class FeedbackController extends BaseController {
             $feedback_mail = Config::get('mail.feedback.call_address');
             Config::set('mail.sendto_mail', $feedback_mail);
 
-            Config::set('mail.sendto_mail', 'vkharseev@gmail.com');
+            #Config::set('mail.sendto_mail', 'vkharseev@gmail.com');
             $this->postSendMessage(NULL, array('subject' => 'Заказ звонка',
                 'phone' => Input::get('phone')), 'call_request');
+            $json_request['responseText'] = 'Сообщение отправлено';
+            $json_request['status'] = TRUE;
+        else:
+            $json_request['responseText'] = 'Неверно заполнены поля';
+            $json_request['responseErrorText'] = implode($validation->messages()->all(), '<br />');
+        endif;
+        return Response::json($json_request, 200);
+    }
+
+    public function requestBron() {
+
+        if (!Request::ajax()) return App::abort(404);
+        $json_request = array('status' => FALSE, 'responseText' => '', 'redirect' => FALSE);
+        $validation = Validator::make(Input::all(), array('id' => 'required', 'name' => 'required',
+            'email' => 'required|email', 'phone' => 'required'));
+        if ($validation->passes()):
+
+            $feedback_mail = Config::get('mail.feedback.bran_address');
+            Config::set('mail.sendto_mail', $feedback_mail);
+
+            #Config::set('mail.sendto_mail', 'vkharseev@gmail.com');
+            $this->postSendMessage(NULL, array('subject' => 'Бронирование участка',
+                'land_id' => Input::get('id'),
+                'name' => Input::get('name'),
+                'email' => Input::get('email'),
+                'phone' => Input::get('phone')
+            ), 'bron_request');
             $json_request['responseText'] = 'Сообщение отправлено';
             $json_request['status'] = TRUE;
         else:
--- app/views/templates/site/overlays.blade.php
+++ app/views/templates/site/overlays.blade.php
@@ -10,8 +10,8 @@
             <div class="block-cont">
                 <div class="book__title"><span>Участок №<span class="js-book-number"></span>, <span class="js-book-line"></span>-ая очередь, <span class="js-book-area"></span> соток</span></div>
                 <div class="overlay__form">
-                    <form id="book-form" class="js-overlay-form" action="theme/build/json/form.json">
-                        <input type="hidden" name="id" class="js-input-book-id">
+                    {{ Form::open(array('route'=>'request_bron','id'=>'book-form','class'=>'js-overlay-form')) }}
+                        {{ Form::hidden('id',NULL,array('class'=>'js-input-book-id')) }}
 
                         <div class="form__block block-phone">
                             <div class="block__title">Ваш номер телефона</div>
@@ -38,7 +38,7 @@
                         <div class="form__block">
                             <div class="block__response js-response-text">Сообщение</div>
                         </div>
-                    </form>
+                    {{ Form::close() }}
                     <div class="overlay__success js-book-success">Скоро с вами свяжутся наши специалисты<br>для уточнения информации</div>
                 </div>
             </div>
