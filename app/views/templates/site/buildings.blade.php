740b91c683bb2aad1d1d26f5867ea9e41b4064d3
Владимир <vkharseev@gmail.com>
2015-09-07 11:18:10 +0300

    Реализовать фильтры в разделе «Построить дом»

--- app/views/templates/site/buildings.blade.php
+++ app/views/templates/site/buildings.blade.php
@@ -36,27 +36,14 @@
     <script>
         Dictionary = window.Dictionary || {};
         Dictionary.buildings = {
-            @if($lands->count())
-                @foreach($lands as $land)
-                    "{{ $land->id }}": {
-                id: {{ $land->id }},
-                number: "{{ $land->number }}",
-                land_area: {{ $land->area }},
-                price: {{ $land->price }},
-                price_total: {{ $land->price_house }},
-                coordinate_x: {{ $land->coordinate_x }},
-                coordinate_y: {{ $land->coordinate_y }},
-                sold: {{ $land->sold }},
-                status: {{ $land->status }},
-                turn: {{ $land->turn }}
-
-
+@if($lands->count())
+    @foreach($lands as $land)
+            "{{ $land->id }}": {
+                id: {{ $land->id }}, number: "{{ $land->number }}", land_area: {{ $land->area }}, price: {{ $land->price }}, price_total: {{ $land->price_house }},
+                coordinate_x: {{ $land->coordinate_x }}, coordinate_y: {{ $land->coordinate_y }}, sold: {{ $land->sold }}, status: {{ $land->status }}, turn: {{ $land->turn }}
             },
-            @endforeach
-        @endif
-
-
-
+    @endforeach
+@endif
         };
     </script>
 @stop
